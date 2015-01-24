6a17ca576d32e95e34e643e4a2b5b28b86fbd40f
Alexandru Pana <astronothing@gmail.com>
2015-01-24 14:06:52 +0200

    Add build.sh script to automate all the steps in the BUILD instructions file

--- BUILD
+++ BUILD
@@ -1,29 +1,38 @@
-# bring in SDL2
+## Build the dependencies
+
+### install the premake build tool
+sudo apt-get install premake4 -y
+
+### install the python scripting language
+sudo apt-get install python -y
+
+### bring in SDL2
 sudo apt-get install libgl1-mesa-dev libsdl2-dev -y
 
-# bring in bgfx
+### bring in the other dependencies
 git submodule init
 git submodule update
 
-# build bgfx
+### build bgfx
 cd extern/bgfx
 make
 make linux-debug64
+cd ../..
 
-# install the premake build tool
-sudo apt-get install premake4 -y
-
-# build and prepare nanovg
+### build and prepare nanovg
 cd extern/nanovg
 premake4 gmake
 cd build
 make nanovg
 cd ..
 mkdir -p include/nanovg
-mv src/*.h include/nanovg
+cp src/*.h include/nanovg
+cd ../..
 
-# install nvidia tools
+## Build the assets
+
+### install nvidia tools
 sudo apt-get install libnvtt-bin -y
 
-# build the assets
-tools/build.py
+### build the assets
+tools/build_assets.py
---
+++ tools/build.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+echo
+echo "------------------------------------------"
+echo "Installing tools and dendencies"
+echo "------------------------------------------"
+echo
+sudo apt-get install python libgl1-mesa-dev libsdl2-dev premake4 libnvtt-bin -y
+
+echo
+echo "------------------------------------------"
+echo "Initializing submodules"
+echo "------------------------------------------"
+echo
+git submodule init
+git submodule update
+
+echo
+echo "------------------------------------------"
+echo "Building bgfx"
+echo "------------------------------------------"
+echo
+cd extern/bgfx
+make .build/projects/gmake-linux
+make linux-debug64
+cd ../..
+
+echo
+echo "------------------------------------------"
+echo "Building nanovg"
+echo "------------------------------------------"
+echo
+cd extern/nanovg
+premake4 gmake
+cd build
+make nanovg
+cd ..
+mkdir -p include/nanovg
+cp src/*.h include/nanovg
+cd ../..
+
+echo
+echo "------------------------------------------"
+echo "Building assets"
+echo "------------------------------------------"
+echo
+python tools/build_assets.py
